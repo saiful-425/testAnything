a4e90e09ea10cc237fc19bc04f87863354524854
Saiful Isalm <islam42555@gmail.com>
2018-04-26 15:56:34 +0600

    starting

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/cruds', 'CrudsController', ['except' => 'edit', 'store', 'show']);
